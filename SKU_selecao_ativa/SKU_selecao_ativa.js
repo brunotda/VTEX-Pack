0c68a8a90bc4a93b2f81e2ca4ba6e2d9d0534274
Carlos Vinicius <carlosjp@hotmail.co.jp>
2012-11-26 17:23:50 -0200

    nova versão 1.1 com suporte a parametros

--- SKU_selecao_ativa/SKU_selecao_ativa.js
+++ SKU_selecao_ativa/SKU_selecao_ativa.js
@@ -1,10 +1,9 @@
 /**
 * VTEX - SKU Seleção Ativa
 * @author Carlos Vinicius
-* @version 1.0
-* @date 2012-11-16
+* @version 1.1
+* @date 2012-11-26
 */
-
 (function($){
 	$.fn.vtexLoadingSku=function(opts)
 	{
@@ -22,6 +21,14 @@
 		
 		_default=
 		{
+			// Define se será utilizado um intervalo de tempo para verificar se o SKU esta pronto.
+			useInterval:true,
+			// Define se a verificação será feita através de "ajaxStop", "ajaxComplete" ou "none" (para não fazer a verificação após as requisições)
+			method:"ajaxComplete",
+			// Checar apenas no evento window.onload
+			onlyWindowOnload:false,
+			// Callback chamado todas as vezes em que o sistema checa se o SKU esta pronto, recebe como parametro uma variável booleane que diz se o SKU esta ativo ou não
+			checkCallback:function(active){}
 		};
 		
 		options=$.extend({},_default,opts);
@@ -36,38 +43,46 @@
 				input.attr("checked",true).trigger("change");
 				if(input.next("label").hasClass("sku-picked"))
 				{
-					input.attr("checked",false)
+					input.attr("checked",false).trigger("change")
 						.next("label").removeClass("sku-picked");
 					$this.hide();
 					on=1;
+					options.checkCallback(on);
 				}
 			}
 			else
 			{
 				$this.hide();
 				on=1;
+				options.checkCallback(on);
 			}
 		};
 		
-		// Checando se o controle de SKU esta pronto a cada 3/4 de segundo
-		interval=setInterval(function(){
-			if(on)
-			{
-				clearInterval(interval);
-				return false;
-			}
-			fn();
-		},750);
-		
-		// Verificando se o controle de SKU esta pronto após o termino de cada requisição ajax
-		$(document).ajaxComplete(function(){
-			fn();
-		});	
+		if(!options.onlyWindowOnload)
+		{
+			// Checando se o controle de SKU esta pronto a cada 3/4 de segundo
+			if(options.useInterval)
+				interval=setInterval(function(){
+					if(on)
+					{
+						clearInterval(interval);
+						return false;
+					}
+					fn();
+				},750);
+			
+			// Verificando se o controle de SKU esta pronto após o termino de cada requisição ajax
+			if(options.method!=="none")
+				$(document)[options.method](function(){
+					fn();
+				});
+		}
 		
 		// Exibindo os SKUs e cancelando a função de monitoramento após o window.onload
 		$(window).load(function(){
 			$this.hide();
 			on=1;
+			options.checkCallback(on);
 		});
 	}
 })(jQuery);
